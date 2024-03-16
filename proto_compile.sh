0efda8046031b11d0a67d588738029a258c36444
Marsh <dsilvamarsh@gmail.com>
2024-03-16 18:31:56 +0000

    Added Pain008 xsd to protobuf conversion

--- proto_compile.sh
+++ proto_compile.sh
@@ -1,8 +1,3 @@
-#protoc  --java_out=./src/main/java/com/bank/core/payments/iso/proto ./src/main/proto/*.proto
-export SRC_DIR=./src/main/proto 
+export SRC_DIR=./src/main/resources
 export DST_DIR=./src/main/java
-#java -jar xsd2thrift/target/xsd2thrift-1.0.jar --protobuf --package=test com-bank-core-payments-iso/src/main/resources/payments_initiation/pain.008.001.10.xsd
-
-
-java -jar xsd2thrift-1.0.jar --protobuf --filename=./src/main/proto/pain8.proto --package=com.bank.core.payments.iso.proto  src/main/resources/payments_initiation/pain.008.001.10.xsd
-protoc -I=$SRC_DIR --java_out=$DST_DIR $SRC_DIR/pain8.proto
+protoc -I=$SRC_DIR --java_out=$DST_DIR $SRC_DIR/proto/*.proto
--- src/main/java/com/bank/core/payments/iso/config/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.bank.core.payments.iso.config;
-
-public class Launcher {
-
-	
-	public static void main(String[] args) {
-		
-	}
-}
