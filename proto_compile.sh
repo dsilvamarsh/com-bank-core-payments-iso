1021c5107d90b678c1d0f450ead95475a3d9a45d
Marsh <dsilvamarsh@gmail.com>
2024-03-18 19:14:11 +0000

    Refactored the folders

--- proto_compile.sh
+++ proto_compile.sh
@@ -1,3 +1,3 @@
-export SRC_DIR=./src/main/resources
+export SRC_DIR=./src/main/resources/pain_008_001_10
 export DST_DIR=./src/main/java
-protoc -I=$SRC_DIR --java_out=$DST_DIR $SRC_DIR/proto/*.proto
+protoc -I=$SRC_DIR --java_out=$DST_DIR $SRC_DIR/*.proto
